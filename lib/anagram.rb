583455c9f17299b2eb79dc7f7b242ddf9b3d019c
Rebekah Zhou <rebekahzhou@gmail.com>
2022-02-24 11:26:18 -0800

    commit tree 5d506c5b7775227c658ea82d927f6bee4de8cb4a

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(anagram_array)
+        anagram_array.filter { |anagram| anagram.chars.sort == @word.chars.sort }
+    end 
+end
+
+listen = Anagram.new("listen")
+listen.match(%w[enlists google inlets banana])
